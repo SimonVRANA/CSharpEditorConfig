ac3ce3a08f00870a74c9030465d1460da80130b8
Simon Vrana <simon.vrana.pro@gmail.com>
2024-05-09 09:43:09 +0200

    [Fct] Configured the .editorconfig. Added a .gitignore. Added small sample of code that should trigger the .editorconfig.

---
+++ CodeExample/TestEditorConfig/TestEditorConfig/Program.cs
@@ -0,0 +1,49 @@
+﻿// This code has been made by Simon VRANA.
+// Please ask by email (simon.vrana.pro@gmail.com) before reusing for commercial purpose.
+
+// This class contains example of code that should be caught by the .editorconfig file
+
+Console.WriteLine("Hello, World!");
+
+int test = 4;
+var test2 = 4;
+
+// Not working: dotnet_style_operator_placement_when_wrapping = beginning_of_line:suggestion
+if (true &&
+	true)
+{
+	test++;
+	test2++;
+}
+
+if (true)
+	test++;
+
+ClassThatCanBeNull? TestNullOperator(ClassThatCanBeNull? classThatCanBeNull)
+
+{
+	if (classThatCanBeNull == null)
+	{
+		classThatCanBeNull = new ClassThatCanBeNull();
+	}
+	if (classThatCanBeNull != null)
+	{
+		classThatCanBeNull.Test();
+	}
+
+	return classThatCanBeNull;
+}
+TestNullOperator(null);
+
+internal class ClassThatCanBeNull
+{
+	private int value;
+
+	public int Test()
+	{
+		var test = value;
+		test++;
+
+		return test;
+	}
+}
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # CSharpEditorConfig
-This is my personal editor config that i'll ber reusing in all my C# projects.
+This is my personal editor config that I'll be reusing in all my C# projects.
+
+To use this, put your projects in children folders.
+
+Enable code cleanup on save (ctrl+Q -> "run code cleanup on save")
